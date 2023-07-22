cd98c8d5261babf6011e28c2a7d3e8beabc6c084
karine <karinedeveloper@gmail.com>
2023-07-22 18:35:45 -0300

    adicionado funcao camelCase

--- src/camelCase.ts
+++ src/camelCase.ts
@@ -5,5 +5,27 @@
  * @returns o texto enviado por parâmetro, alterado para camelCase.
  */
 export const camelCase = (word: string): string => {
-  return '';
+  if (!word || word.length === 0) {
+    return '';
+  }
+
+  let result = '';
+  let nextCapital = false;
+
+  for (let i = 0; i < word.length; i++) {
+    const char = word[i];
+
+    if (char === ' ' || char === '-' || char === '_') {
+      nextCapital = true;
+    } else {
+      if (nextCapital) {
+        result += char.toUpperCase();
+        nextCapital = false;
+      } else {
+        result += char.toLowerCase();
+      }
+    }
+  }
+
+  return result;
 };
--- src/capitalize.ts
+++ src/capitalize.ts
@@ -5,5 +5,12 @@
  * @returns o texto enviado por parâmetro, alterado para capitalize.
  */
 export const capitalize = (text: string): string => {
-  return '';
+  if (text.length === 0) {
+    return text;
+  }
+
+  const firstLetter = text.charAt(0).toUpperCase();
+  const restOfTheText = text.slice(1).toLowerCase();
+
+  return firstLetter + restOfTheText;
 };
