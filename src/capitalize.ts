77f86503c04e77dfc1c88223bb5977b7becb9065
karine <karinedeveloper@gmail.com>
2023-07-22 18:37:33 -0300

    adicionado funcao capitalize

--- src/capitalize.ts
+++ src/capitalize.ts
@@ -8,9 +8,7 @@ export const capitalize = (text: string): string => {
   if (text.length === 0) {
     return text;
   }
-
   const firstLetter = text.charAt(0).toUpperCase();
   const restOfTheText = text.slice(1).toLowerCase();
-
   return firstLetter + restOfTheText;
 };
