8f9098b8f336e68304fe87a3ae7e8681d81dc9c9
Paulo Fernandes <phfernandespereira@gmail.com>
2023-07-21 22:12:33 -0300

    implementa metodo de chunk de arrays

--- src/chunk.ts
+++ src/chunk.ts
@@ -11,5 +11,43 @@
  * @returns
  */
 export const chunk = (valores: number[], tamanho: number): number[][] => {
-  return null;
+  // 1. preciso iterar em todo o meu array de entrada de dados.
+  // 2. para cada laço, eu preciso agrupar os elementos em novos arrays, com o tamanho máximo da variavel "tamanho"
+  // 3. preciso retornar um array contendo estes arrays agrupados.
+
+  // 1. para cada valor de valores
+  // 2.1 preciso criar um array com o tamanho máximo de "tamanho", caso este array ainda não exista.
+  // 2.2 se ele existir, devo apenas adicionar os valores neste array, até que ele tenha um tamanho máximo permitido.
+  // 2.3 se ele existir e o tamanho for superado, devo adicionar um novo array com o elemento atual no meu array de resultado.
+
+  // valores   [1, 2, 3, 4, 5]
+  // valor     1
+  // tamanho   2
+  // resultado [[]]
+  // chunkAtual []
+
+  const resultado: number[][] = [];
+
+  // 1. para cada valor de valores
+  for (const valor of valores) {
+    let chunkAtual: number[] = resultado[resultado.length - 1];
+    if (chunkAtual === undefined) {
+      chunkAtual = [];
+      resultado.push(chunkAtual);
+    }
+
+    // 2.2 se ele existir, devo apenas adicionar os valores neste array, até que ele tenha um tamanho máximo permitido.
+    // 2.3 se ele existir e o tamanho for superado, devo adicionar um novo array com o elemento atual no meu array de resultado.
+    if (chunkAtual.length < tamanho) {
+      chunkAtual.push(valor);
+    } else {
+      resultado.push([valor]);
+    }
+
+    console.log(chunkAtual);
+  }
+
+  return resultado;
 };
+
+console.log(chunk([1, 2, 3, 4, 5], 2));
