d21d10100aa3919a8d3c9e5c25d37f990b1e25aa
Paulo Fernandes <phfernandespereira@gmail.com>
2023-07-21 21:50:51 -0300

    implementa método de criação de range de números.

--- src/range.ts
+++ src/range.ts
@@ -8,4 +8,19 @@
  * @param size
  * @param start
  */
-export const range = (size: number, start = 0) => {};
+export const range = (size: number, start = 0) => {
+  // definir o numero inicial
+  // definir o tamanho do array
+  // a partir do numero inicial, devo repetir a construcao de um novo numero no meu array de resultado, até que alcance o tamanho do resultado.
+
+  // a partir do zero, até o tamanho do array
+  //  adiciono um valor ao meu array de resultado.
+
+  const resultado = [];
+
+  for (let i = 0; i < size; i++) {
+    resultado.push(i + start);
+  }
+
+  return resultado;
+};
