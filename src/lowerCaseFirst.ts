a362c2cb6b6465be6fc374be582f8e214ad515f6
karine <karinedeveloper@gmail.com>
2023-07-24 10:16:32 -0300

    adiciona função lowerCaseFirst.ts

--- src/lowerCaseFirst.ts
+++ src/lowerCaseFirst.ts
@@ -1,1 +1,7 @@
-export const lowerCaseFirst = (text: string): string => text;
+export const lowerCaseFirst = (text: string): string => {
+  if (text.length === 0) {
+    return text;
+  }
+
+  return text.charAt(0).toLowerCase() + text.substring(1);
+};
