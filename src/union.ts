3a0212154377fb6ef13e3680404336522df4c2ee
karine <karinedeveloper@gmail.com>
2023-07-24 10:26:29 -0300

    adiciona função union.ts

--- src/union.ts
+++ src/union.ts
@@ -8,6 +8,32 @@
  *
  * @returns somente os itens definidos.
  */
-export const union = <T>(...args: T[]): T[] => {
-  return null;
+// export const union = <T>(...args: T[]): T[] => {
+//   return null;
+// };
+
+export const union = <T extends any[]>(...args: T[]): T => {
+  const uniqueItems: T = [] as T;
+
+  for (let i = 0; i < args.length; i++) {
+    const arr = args[i];
+
+    for (let j = 0; j < arr.length; j++) {
+      const item = arr[j];
+      let isDuplicate = false;
+
+      for (let k = 0; k < uniqueItems.length; k++) {
+        if (item === uniqueItems[k]) {
+          isDuplicate = true;
+          break;
+        }
+      }
+
+      if (!isDuplicate) {
+        uniqueItems.push(item);
+      }
+    }
+  }
+
+  return uniqueItems;
 };
