55bce48ff5b20b87155854c2443d98f55c8dc12e
karine <karinedeveloper@gmail.com>
2023-07-24 10:14:09 -0300

    adiciona função intersection.ts

--- src/intersection.ts
+++ src/intersection.ts
@@ -8,5 +8,32 @@
  * @returns somente os itens definidos.
  */
 export const intersection = (left: number[], right: number[]): number[] => {
-  return null;
+  const result: number[] = [];
+
+  for (let i = 0; i < left.length; i++) {
+    let found = false;
+
+    for (let j = 0; j < right.length; j++) {
+      if (left[i] === right[j]) {
+        found = true;
+        break;
+      }
+    }
+
+    if (found) {
+      let isDuplicate = false;
+      for (let k = 0; k < result.length; k++) {
+        if (left[i] === result[k]) {
+          isDuplicate = true;
+          break;
+        }
+      }
+
+      if (!isDuplicate) {
+        result.push(left[i]);
+      }
+    }
+  }
+
+  return result;
 };
