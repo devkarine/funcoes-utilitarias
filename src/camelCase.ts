a1c7b7ae19961ea32870571530502ebff8f26f01
karine <karinedeveloper@gmail.com>
2023-07-24 10:06:34 -0300

    alterada função camelCase.ts

--- src/camelCase.ts
+++ src/camelCase.ts
@@ -5,27 +5,5 @@
  * @returns o texto enviado por parâmetro, alterado para camelCase.
  */
 export const camelCase = (word: string): string => {
-  if (!word || word.length === 0) {
-    return '';
-  }
-
-  let result = '';
-  let nextCapital = false;
-
-  for (let i = 0; i < word.length; i++) {
-    const char = word[i];
-
-    if (char === ' ' || char === '-' || char === '_') {
-      nextCapital = true;
-    } else {
-      if (nextCapital) {
-        result += char.toUpperCase();
-        nextCapital = false;
-      } else {
-        result += char.toLowerCase();
-      }
-    }
-  }
-
-  return result;
+  return word.replace(/[^a-zA-Z0-9]+(.)/g, (_, char) => char.toUpperCase()).replace(/^[^a-zA-Z0-9]+(.)/g, (_, char) => char.toLowerCase());
 };
